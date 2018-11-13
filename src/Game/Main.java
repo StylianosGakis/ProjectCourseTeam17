af886113f05272a9027951ea68fd81a2e2fff578
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2018-11-13 20:52:36 +0100

    add main method

---
+++ src/Game/Main.java
@@ -0,0 +1,12 @@
+package Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Main myApp = new Main();
+        myApp.runMyProgram();
+    }
+
+    public void runMyProgram() {
+
+    }
+}
